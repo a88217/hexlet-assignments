f12e5b0faff7e125313616555ecd3a779417a97a
@hexlet/cli <support@hexlet.io>
2023-08-29 19:16:38 +0300

    submit java-collections-ru/lists

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,25 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+// BEGIN
+public class App {
+    public static boolean scrabble(String letters, String word) {
+        String[] lettersArray = letters.toLowerCase().split("");
+        String[] wordLettersArray = word.toLowerCase().split("");
+        List<String> lettersList = Arrays.asList(lettersArray);
+        List<String> wordLetters = Arrays.asList(wordLettersArray);
+        boolean result = true;
+        for (String s : wordLetters) {
+            if (lettersList.contains(s)) {
+                lettersList.remove(s);
+            } else {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
+//END
